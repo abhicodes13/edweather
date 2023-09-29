78880b52108966bc857000d72ec0ea45e33f63a7
AbhiCodes <chhettriabhishek9@gmail.com>
2023-09-29 18:34:09 -0400

    API

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 async function getWeather(){
-    
     const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=40.522%2C-74.41';
 const options = {
 	method: 'GET',
@@ -12,11 +11,15 @@ const options = {
 try {
 	const response = await fetch(url, options);
 	const result = await response.json();
-    const data = await result.current.temp_f;
-
-	console.log(data);
+    const data = result.current.temp_f;
+    const name = result.location.name;
+    const region = result.location.region;
+    
+    console.log(name,data,region);
+    document.querySelector(".card").innerHTML=`<h3>${name}, ${region}</h3><h1>${data}</h1>`;
+	
 } catch (error) {
 	console.error(error);
 }
 }
-getWeather()
+getWeather();
